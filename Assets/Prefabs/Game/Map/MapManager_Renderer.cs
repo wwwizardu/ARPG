6e51c4b006ad47c30d695b9b32f7e6404d20ecee
bhleesanghoon <peacekp@krafton.com>
2025-08-15 15:43:31 +0900

    MapManager_Renderer 클래스 추가

---
+++ Assets/Prefabs/Game/Map/MapManager_Renderer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace ARPG.Map
+{
+    public partial class MapManager : MonoBehaviour
+    {
+        // Add any additional methods or properties specific to the MapManager here
+    }
+}
+
